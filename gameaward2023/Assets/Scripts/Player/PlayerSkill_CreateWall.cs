b018233c2f64634ba6713afc33a9c7072129c274
AT13A24SaitoRyota <nhs.saitoryota@gmail.com>
2023-04-17 14:52:56 +0900

    プレイヤースキル更新

--- gameaward2023/Assets/Scripts/Player/PlayerSkill_CreateWall.cs
+++ gameaward2023/Assets/Scripts/Player/PlayerSkill_CreateWall.cs
@@ -17,6 +17,12 @@ public class PlayerSkill_CreateWall : MonoBehaviour
     private bool bMapLoading = false;           // マップがロードされたか
     // -------------------------------------------------------------------
 
+    // スキル関連 --------------------------------------------------------
+    private GameObject TargetObj = null;        // タップ対象のオブジェクト
+    [SerializeField]
+    private bool bSkill = false;
+    // -------------------------------------------------------------------
+
     void Start()
     {
         PlayerObj = GameObject.Find("Player");
@@ -37,10 +43,64 @@ public class PlayerSkill_CreateWall : MonoBehaviour
             bMapLoading = true;
         }
 
-        // プレイヤーが動いていないとき
-        if (!PlayerMovement.IsMoving)
-        {
-
+        // プレイヤーが動いていないとき かつ プレイヤーが４歩以上移動したときのみ発動可能
+        if(!PlayerMovement.IsMoving && PlayerMovement.StepCount >= 4)
+        { 
+            bSkill = true;
         }
+        else
+        {
+            bSkill = false;
+        }
+
+        if(bSkill)
+        {
+            // クリック検出処理
+            if (Input.GetMouseButtonDown(0))
+            {
+                // 歩数リセット
+                PlayerMovement.StepCount = 0;
+
+                // クリックしたブロックを障害物に変更
+                GetBlockTapPos();
+
+                bSkill = false;
+            }
+        }
+    }
+
+    private void GetBlockTapPos()
+    {
+        // クリックした方向にカメラからRayを飛ばす
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit = new RaycastHit();
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            // Rayに当たる位置に存在するオブジェクトを取得
+            TargetObj = hit.collider.gameObject;
+        }
+
+        // 対象オブジェクト(ブロック)が存在する場合の処理
+        if (TargetObj != null)
+        {
+            // ブロック選択時処理
+            ChangeBlock(TargetObj.GetComponent<MapBlock>());
+        }
+        else
+        {
+            Debug.Log("指定の場所には使えません");
+        }
+    }
+
+    private void ChangeBlock(MapBlock targetBlock)
+    {
+        // 選択した地面を障害物に変更
+        map[(int)targetBlock.transform.position.x, (int)targetBlock.transform.position.z] = 2;
+    }
+
+    public int[,] GetUpdateMap()
+    {
+        return map;
     }
 }
