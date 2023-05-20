96ea70bc7c80e8ff2700b565f5009225fe98718e
suecha888 <game.create.programmer@gmail.com>
2023-05-20 15:47:10 +0900

    カメラの倍率調整を可能にしました。カメラの角度を調節。

--- gameaward2023/Assets/Scripts/Camera/PlayerCameraFollow.cs
+++ gameaward2023/Assets/Scripts/Camera/PlayerCameraFollow.cs
@@ -5,14 +5,21 @@ using UnityEngine;
 public class PlayerCameraFollow : MonoBehaviour
 {
     private GameObject target;
-    private Vector3 offset;
+    [SerializeField]
+    private Vector3 offset = new Vector3(0.0f, 3.0f, -2.5f);
+    [SerializeField]
+    private Vector3 offset3rd = new Vector3(0.0f, 8.0f, 0.0f);
+    private Quaternion Angle = Quaternion.Euler(50.0f, 0.0f, 0.0f);
+    private int magnificationIdx = 0;
+    [SerializeField]
+    private int magnificationIdxMax = 3;
+    [SerializeField]
+    private float magnification =1.0f;
 
     void Start()
     {
+        gameObject.transform.rotation = Angle;
         target = GameObject.Find("Player");
-
-        // ゲーム開始時点のカメラとターゲットの距離（オフセット）を取得
-        offset = gameObject.transform.position - target.transform.position;
     }
 
     /// <summary>
@@ -20,7 +27,28 @@ public class PlayerCameraFollow : MonoBehaviour
     /// </summary>
     void LateUpdate()
     {
+        // カメラの倍率調整
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            gameObject.transform.rotation = Angle;
+            magnificationIdx = (magnificationIdx + 1) % (magnificationIdxMax+1);
+            magnification = 1 + magnificationIdx * 0.5f;
+            // 上から視点
+            if(magnificationIdx == magnificationIdxMax)
+            {
+                magnification = 1 + magnificationIdxMax * 0.5f;
+                gameObject.transform.rotation = Quaternion.Euler(90.0f,0.0f,0.0f);
+            }
+        }
+
         // カメラの位置をターゲットの位置にオフセットを足した場所にする。
-        gameObject.transform.position = target.transform.position + offset;
+        if(magnificationIdx == magnificationIdxMax)
+        {
+            gameObject.transform.position = target.transform.position + offset3rd;
+        }
+        else
+        {
+            gameObject.transform.position = target.transform.position + (offset * magnification);
+        }
     }
 }
