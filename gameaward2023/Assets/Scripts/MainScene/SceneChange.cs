bd689e5d7d47787a576d5aa21c488f69e5e92d41
Ikurun7810 <95463647+Ikurun7810@users.noreply.github.com>
2023-06-12 12:42:26 +0900

    シーンチェンジの修正

--- gameaward2023/Assets/Scripts/MainScene/SceneChange.cs
+++ gameaward2023/Assets/Scripts/MainScene/SceneChange.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
 
 public class SceneChange : MonoBehaviour
 {
@@ -10,34 +11,49 @@ public class SceneChange : MonoBehaviour
     public string RetryStage;
     public string NextStage;
     public AudioClip WarpSound;
+    public AudioClip FailedSound;
     public AudioClip RetrySound;
     AudioSource SceneAudioSource;
     private bool OnlyClear;
+    private bool OnlyFailed;
+    private bool OnlyCheck;
     public float FadeTime = 1.5f;
     // Start is called before the first frame update
+    private Controller _gameInputs;    //gamepad
+
     void Start()
     {
         SceneAudioSource = GetComponent<AudioSource>();
         PlayerData = GameObject.Find("Player");
         PlayerStatus = PlayerData.GetComponent<PlayerMovement>();
+
+        //Input Actionインスタンス生成
+        _gameInputs = new Controller();
+
+        //Actionイベント登録
+        _gameInputs.Submit.Submit.performed += OnSelect;
+
+        _gameInputs.Enable();
+
         OnlyClear = false;
+        OnlyFailed = false;
+        OnlyCheck = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(PlayerStatus.IsTouched)
+        if (PlayerStatus.IsTouched)
         {
-            if (Input.GetKeyDown(KeyCode.Return))
+            if (!OnlyFailed)
             {
-                FadeManager.Instance.LoadScene(RetryStage, 1.0f);
-                SceneAudioSource.PlayOneShot(RetrySound);
+                SceneAudioSource.PlayOneShot(FailedSound);
+                OnlyFailed = true;
             }
         }
-        if(PlayerStatus.ClearCheck&&!PlayerStatus.IsTouched && !OnlyClear)
+        if (PlayerStatus.ClearCheck && !PlayerStatus.IsTouched && !OnlyClear)
         {
-
-            FadeManager.Instance.LoadScene(NextStage,FadeTime);
+            FadeManager.Instance.LoadScene(NextStage, FadeTime);
             SceneAudioSource.PlayOneShot(WarpSound);
             OnlyClear = true;
         }
@@ -47,5 +63,15 @@ public class SceneChange : MonoBehaviour
             FadeManager.Instance.LoadScene("TitleScene", 1.0f);
         }
 
+    }
+    private void OnSelect(InputAction.CallbackContext context)
+    {
+
+        if (PlayerStatus.IsTouched && !OnlyCheck)
+        {
+            FadeManager.Instance.LoadScene(RetryStage, 1.0f);
+            SceneAudioSource.PlayOneShot(RetrySound);
+            OnlyCheck = true;
+        }
     }
 }
